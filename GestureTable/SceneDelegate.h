ff995f4e02229f05fdf97eadacd856e0c4d7d1e6
JSH-00 <jshjshh@163.com>
2020-09-12 13:58:46 +0800

    Initial Commit

---
+++ GestureTable/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  GestureTable
+//
+//  Created by JSH on 2020/9/12.
+//  Copyright © 2020 JSH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ GestureTable/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GestureTable
+//
+//  Created by JSH on 2020/9/12.
+//  Copyright © 2020 JSH. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
